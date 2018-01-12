57839c109e500d9561b20220edc1047a3b0911ba
oaps <35172365+oaps@users.noreply.github.com>
2018-01-11 20:24:01 -0500

    Update passValue.js

--- passValue.js
+++ passValue.js
@@ -54,7 +54,7 @@ var demo123CF_CopyFieldValue = new function() {
 
 $( document ).ready(function() {
     demo123CF_CopyFieldValue.setFieldOriginID('36073643');
-    demo123CF_CopyFieldValue.setFieldDestinationID('36148866');
+    demo123CF_CopyFieldValue.setFieldDestinationID('36041339');
 	
     demo123CF_CopyFieldValue.init();
 });
