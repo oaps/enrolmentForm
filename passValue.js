6d8dbd6cb5bf81b78c87000dec3088932cfe0cb4
oaps <35172365+oaps@users.noreply.github.com>
2018-01-11 19:51:19 -0500

    Update passValue.js

--- passValue.js
+++ passValue.js
@@ -4,17 +4,25 @@ var demo123CF_CopyFieldValue = new function() {
     this.errorMessage = '';
 
     this.id123cf = '#id123-control';
-    this.fieldOrigin = '';
-    this.fieldDestination = '';
-
+    this.fieldOrigin1 = '';
+    this.fieldOrigin2 = '';
+    this.fieldDestination1 = '';
+    this.fieldDestination2 = '';
+    this.fieldDestination3 = '';
+    this.fieldDestination4 = '';
+    this.fieldDestination5 = '';
+	
     this.setFieldOriginID = function ( fieldId1, fieldId2 ) {
         this.fieldOrigin1 = fieldId1;
         this.fieldOrigin2 = fieldId2;
     }
 
-    this.setFieldDestinationID = function ( fieldId1, fieldId2 ) {
+    this.setFieldDestinationID = function ( fieldId1, fieldId2, fieldId3, fieldId4, fieldId5 ) {
         this.fieldDestination1 = fieldId1;
-        this.fieldDestination2 = fieldId2;	
+        this.fieldDestination2 = fieldId2;
+        this.fieldDestination2 = fieldId3;
+        this.fieldDestination2 = fieldId4;
+        this.fieldDestination2 = fieldId5;	
     }
 
 
@@ -23,10 +31,20 @@ var demo123CF_CopyFieldValue = new function() {
     }
 
     this.validateData = function () {
-        if(!$(this.id123cf+this.fieldOrigin).length)
+        if(!$(this.id123cf+this.fieldOrigin1).length)
             this.errorMessage = 'Invalid origin field';
-        if(!$(this.id123cf+this.fieldDestination).length)
-            this.errorMessage = 'Invalid destination field';
+        if(!$(this.id123cf+this.fieldOrigin2).length)
+            this.errorMessage = 'Invalid origin field';
+        if(!$(this.id123cf+this.fieldDestination1).length)
+            this.errorMessage = 'Invalid destination field1';
+        if(!$(this.id123cf+this.fieldDestination2).length)
+            this.errorMessage = 'Invalid destination field2';
+        if(!$(this.id123cf+this.fieldDestination3).length)
+            this.errorMessage = 'Invalid destination field3';
+        if(!$(this.id123cf+this.fieldDestination4).length)
+            this.errorMessage = 'Invalid destination field4';
+        if(!$(this.id123cf+this.fieldDestination5).length)
+            this.errorMessage = 'Invalid destination field5';
 
         return this.errorMessage == '';
     }
@@ -39,6 +57,9 @@ var demo123CF_CopyFieldValue = new function() {
 
         $(this.id123cf+this.fieldDestination1).val($(this.id123cf+this.fieldOrigin1).val());
         $(this.id123cf+this.fieldDestination2).val($(this.id123cf+this.fieldOrigin2).val());
+	$(this.id123cf+this.fieldDestination3).val($(this.id123cf+this.fieldOrigin3).val());
+	$(this.id123cf+this.fieldDestination4).val($(this.id123cf+this.fieldOrigin4).val());
+	$(this.id123cf+this.fieldDestination5).val($(this.id123cf+this.fieldOrigin5).val());
     }
 
     this.setOnChange = function () {
