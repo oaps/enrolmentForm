1d72eef59af4738143a53f7f7666e41c5ec955fe
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 07:55:51 -0400

    Create oaps_copyvals.js

---
+++ oaps_copyvals.js
@@ -0,0 +1,37 @@
+var Oaps_CopyFieldValue = new function() {
+    var self = this;
+
+    this.errorMessage = '';
+
+    this.fieldOrigin1 = '';
+    this.fieldDestination1 = '';
+	
+
+    this.setFieldOriginID1 = function ( fieldId1 ) {
+        this.fieldOrigin1 = fieldId1;
+    }
+
+   this.setFieldDestinationID1 = function ( fieldId1 ) {
+        this.fieldDestination1 = fieldId1;
+    }
+
+
+    this.init = function () {
+        this.setOnChange();
+    }
+
+    this.setOnChange = function () {
+
+        $(this.fieldOrigin1).change(function() {
+            self.updateDestinationValue1();
+        });
+    }
+}
+
+
+$( document ).ready(function() {
+    Oaps_CopyFieldValue.setFieldOriginID1('36040704');
+    Oaps_CopyFieldValue.setFieldDestinationID1('44541693');
+
+    Oaps_CopyFieldValue.init();
+});
