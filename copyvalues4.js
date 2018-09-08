d229eb3c94071b8755325bf13c38618910a6b13b
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 07:43:08 -0400

    Update and rename copyvalues3.js to copyvalues4.js

--- copyvalues4.js
+++ copyvalues4.js
@@ -2,8 +2,8 @@
  
     window.addEventListener('load', function(){
 
-        var sourceControlId = 36040704-1, /** THE ID OF THE SENDER CONTROL */
-            targetControlId = 36073643, /** THE ID OF THE RECEIVER CONTROL **/
+        var sourceControlId = 36040704, /** THE ID OF THE SENDER CONTROL */
+            targetControlId = 44541693, /** THE ID OF THE RECEIVER CONTROL **/
             sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
             targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
