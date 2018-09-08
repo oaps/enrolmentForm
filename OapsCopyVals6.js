88a1880afb1abc4b38b606bb02c31475d1d7662f
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 08:25:10 -0400

    Update and rename OapsCopyVals5.js to OapsCopyVals6.js

--- OapsCopyVals6.js
+++ OapsCopyVals6.js
@@ -26,8 +26,8 @@ var oaps_CopyFieldValue = new function() {
  
      window.addEventListener('load', function(){
 
-        var sourceControlId = 36040704,
-            targetControlId = 44541819,
+        var sourceControlId = this.fieldOrigin1,
+            targetControlId = this.fieldDestination1,
             sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
             targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
