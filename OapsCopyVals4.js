c7be86058c38a8dcb4ed22e96c778bfe4675427e
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 08:13:39 -0400

    Update and rename OapsCopyVals3.js to OapsCopyVals4.js

--- OapsCopyVals4.js
+++ OapsCopyVals4.js
@@ -38,16 +38,12 @@ var Oaps_CopyFieldValue = new function() {
     }
 
 
-    this.init = function () {
-        this.setOnChange();
-    }
-
     this.setOnChange = function(){
  
     window.addEventListener('load', function(){
 
-        var sourceControlId = this.fieldOrigin1, /** THE ID OF THE SENDER CONTROL */
-            targetControlId = this.fieldDestination1, /** THE ID OF THE RECEIVER CONTROL **/
+        var sourceControlId = 36040704,
+            targetControlId = 44541819,
             sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
             targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
@@ -60,15 +56,6 @@ var Oaps_CopyFieldValue = new function() {
     });
 
 }
-
-
-
-$( document ).ready(function() {
-    Oaps_CopyFieldValue.setFieldOriginID1('36040704');
-    Oaps_CopyFieldValue.setFieldDestinationID1('44541693');
-
-    Oaps_CopyFieldValue.init();
-});
 
 
 $( document ).ready(function() {
