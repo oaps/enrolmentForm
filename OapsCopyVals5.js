ecda9c591b8a79874a09974de7a7a37b7bf46f0c
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 08:21:15 -0400

    Update and rename OapsCopyVals4.js to OapsCopyVals5.js

--- OapsCopyVals5.js
+++ OapsCopyVals5.js
@@ -1,46 +1,30 @@
-var Oaps_CopyFieldValue = new function() {
+var oaps_CopyFieldValue = new function() {
     var self = this;
 
     this.errorMessage = '';
 
     this.fieldOrigin1 = '';
+   
     this.fieldDestination1 = '';
-	
+   	
 
     this.setFieldOriginID1 = function ( fieldId1 ) {
         this.fieldOrigin1 = fieldId1;
     }
-
-   this.setFieldDestinationID1 = function ( fieldId1 ) {
+    
+    
+    this.setFieldDestinationID1 = function ( fieldId1 ) {
         this.fieldDestination1 = fieldId1;
     }
-
 
     this.init = function () {
         this.setOnChange();
     }
-
-var Oaps_CopyFieldValue = new function() {
-    var self = this;
-
-    this.errorMessage = '';
-
-    this.fieldOrigin1 = '';
-    this.fieldDestination1 = '';
-	
-
-    this.setFieldOriginID1 = function ( fieldId1 ) {
-        this.fieldOrigin1 = fieldId1;
-    }
-
-   this.setFieldDestinationID1 = function ( fieldId1 ) {
-        this.fieldDestination1 = fieldId1;
-    }
-
+    
 
     this.setOnChange = function(){
  
-    window.addEventListener('load', function(){
+     window.addEventListener('load', function(){
 
         var sourceControlId = 36040704,
             targetControlId = 44541819,
@@ -53,14 +37,14 @@ var Oaps_CopyFieldValue = new function() {
 
         targetControlInstance.setValue( sourceControlInstance.getValue() );
 
-    });
+     });
 
+   }
 }
 
-
 $( document ).ready(function() {
-    Oaps_CopyFieldValue.setFieldOriginID1('36040704');
-    Oaps_CopyFieldValue.setFieldDestinationID1('44541819');
+    oaps_CopyFieldValue.setFieldOriginID1('36040704');
+    oaps_CopyFieldValue.setFieldDestinationID1('44541819');
 
-    Oaps_CopyFieldValue.init();
+    oaps_CopyFieldValue.init();
 });
