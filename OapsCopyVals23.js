41fdde669be2df717a100a7d11e73860eb7df98b
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 17:15:08 -0400

    Update and rename OapsCopyVals22.js to OapsCopyVals23.js

--- OapsCopyVals23.js
+++ OapsCopyVals23.js
@@ -1,33 +1,16 @@
 var oaps_CopyFieldValue = new function() {
     var self = this;
-
-    this.errorMessage = '';
-
-    this.fieldOrigin1 = '36040704';
-   
-    this.fieldDestination1 = '44541819';
-   	
-
-    this.setFieldOriginID1 = function ( fieldId1 ) {
-        this.fieldOrigin1 = fieldId1;
-    }
     
-    
-    this.setFieldDestinationID1 = function ( fieldId1 ) {
-        this.fieldDestination1 = fieldId1;
-    }
-
     this.init = function () {
         this.setOnChange();
     }
     
-
-    this.setOnChange = function(){
+   this.setOnChange = function () {
  
     window.addEventListener('load', function(){
 
-        var sourceControlId = 36040704,
-            targetControlId = 44541819,
+         var sourceControlId = 36040704,
+            targetControlId = 44541819, 
             sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
             targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
@@ -38,8 +21,7 @@ var oaps_CopyFieldValue = new function() {
         targetControlInstance.setValue( sourceControlInstance.getValue() );
 
     });
-
-}
+  }
 }
 
 $( document ).ready(function() {
