648ae674e8f22aa620eb61cb5bf732b947645c2d
oaps <35172365+oaps@users.noreply.github.com>
2018-09-08 08:50:11 -0400

    Update OapsCopyVals9.js

--- OapsCopyVals9.js
+++ OapsCopyVals9.js
@@ -1,33 +1,15 @@
 var oaps_CopyFieldValue = new function() {
-    var self = this;
 
     
-    this.init = function () {
-        this.setOnChange('36040704', '44541819');
-    }
-    
 
-    this.setOnChange = function( source, dest){
+    this.setOnChange = function(source, target){
  
-     window.addEventListener('load', function(){
-
-        var sourceControlId = source,
-            targetControlId = dest,
-            sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
-            targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
-
-        sourceControlInstance.on('value-change', function(){
-            targetControlInstance.setValue( sourceControlInstance.getValue() );
-        });
-
-        targetControlInstance.setValue( sourceControlInstance.getValue() );
-
-     });
+	document.getElementById(source).value = document.getElementById(target).value;
 
    }
 }
 
 $( document ).ready(function() {
-
-    oaps_CopyFieldValue.init();
+ 
+    oaps_CopyFieldValue.setOnChange();
 });
