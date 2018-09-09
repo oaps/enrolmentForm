6ae2d8fb2c8c739c0ad15a9fc0fca4614fb126ae
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 00:54:56 -0400

    Update and rename OapsCopyVals40.js to OapsCopyVals41.js

--- OapsCopyVals41.js
+++ OapsCopyVals41.js
@@ -4,34 +4,31 @@ var demo123CF_CopyFieldValue = new function() {
 	this.outField = new Number();
 
 
-    this.init = function () {
-        this.setOnChange();
+    this.init = function (in,out) {
+        this.setOnChange(in,out);
     }
     
 
-    this.setOnChange = function(){
+    this.setOnChange = function(in,out){
  
-    window.addEventListener('load', function(){
+    window.addEventListener('load', function(in, out){
 
-	    
 	    this.inChildName = 36040704;
 	    this.inSignor = 36935922;
-	    this.relation = 36935922;
-	    console.log(this.inField);
+	    this.relation = 36936048;
+	    console.log("****HERE****");
+	    console.log(out);
 
 	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inChildName),
             targetControlInstance = loader.getEngine().getDocument().getElementById(36181440);
         sourceControlInstance.on('value-change', function(){
             targetControlInstance.setValue( sourceControlInstance.getValue() );
         }); targetControlInstance.setValue( sourceControlInstance.getValue() );
-	    
-
 	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inSignor),
             targetControlInstance = loader.getEngine().getDocument().getElementById(36181455);
         sourceControlInstance.on('value-change', function(){
             targetControlInstance.setValue( sourceControlInstance.getValue() );
         }); targetControlInstance.setValue( sourceControlInstance.getValue() );
-
 	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.relation),
             targetControlInstance = loader.getEngine().getDocument().getElementById(36181458);
         sourceControlInstance.on('value-change', function(){
