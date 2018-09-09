cf280464fd8a7c98c654600219e9b5582b793e13
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 10:21:19 -0400

    Update and rename OapsCopyVals51.js to OapsCopyVals52.js

---
+++ OapsCopyVals52.js
@@ -0,0 +1,45 @@
+var Oaps_CopyFieldValue = new function() {
+    var self = this;
+	
+    this.init = function (){
+        this.setOnChange();
+    }
+    
+    this.setOnChange = function(){
+ 
+    window.addEventListener('load', function(){
+
+	    this.inChildName = 36040704;
+	    this.inSignor = 36935922;
+	    this.relation = 36936048;
+	    
+        var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inChildName),
+            targetControlInstance = loader.getEngine().getDocument().getElementById(36181440);
+        sourceControlInstance.on('value-change', function(){
+            targetControlInstance.setValue( sourceControlInstance.getValue() );
+        });
+        targetControlInstance.setValue( sourceControlInstance.getValue() );
+    	});
+	
+	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inSignor),
+            targetControlInstance = loader.getEngine().getDocument().getElementById(36181455);
+        sourceControlInstance.on('value-change', function(){
+            targetControlInstance.setValue( sourceControlInstance.getValue() );
+        });
+        targetControlInstance.setValue( sourceControlInstance.getValue() );
+    	});
+	
+	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.relation),
+            targetControlInstance = loader.getEngine().getDocument().getElementById(36181458);
+        sourceControlInstance.on('value-change', function(){
+            targetControlInstance.setValue( sourceControlInstance.getValue() );
+        });
+        targetControlInstance.setValue( sourceControlInstance.getValue() );
+    	});
+
+  }
+}
+
+$( document ).ready(function() {
+    Oaps_CopyFieldValue.init(); 
+});
