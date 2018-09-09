97316182eca61f8cb2d8a9ed51aa0c53d07842aa
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 01:01:00 -0400

    Update and rename OapsCopyVals41.js to OapsCopyVals42.js

--- OapsCopyVals42.js
+++ OapsCopyVals42.js
@@ -3,21 +3,23 @@ var demo123CF_CopyFieldValue = new function() {
 	this.inField = new Number();
 	this.outField = new Number();
 
-
-    this.init = function (in,out) {
-        this.setOnChange(in,out);
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+	
+    this.init = function () {
+        this.setOnChange();
     }
     
 
-    this.setOnChange = function(in,out){
+    this.setOnChange = function(){
  
-    window.addEventListener('load', function(in, out){
+    window.addEventListener('load', function(){
 
 	    this.inChildName = 36040704;
 	    this.inSignor = 36935922;
 	    this.relation = 36936048;
 	    console.log("****HERE****");
-	    console.log(out);
 
 	var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inChildName),
             targetControlInstance = loader.getEngine().getDocument().getElementById(36181440);
@@ -42,6 +44,7 @@ var demo123CF_CopyFieldValue = new function() {
 
 $( document ).ready(function() {
 
-    demo123CF_CopyFieldValue.init();
+/**    demo123CF_CopyFieldValue.init(); **/
+	demo123CF_CopyFieldValue.greet('Daljit');
 	
 });
