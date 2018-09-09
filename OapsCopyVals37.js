9c132b9683995538f19fd7f2335b726df2eff865
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 00:04:36 -0400

    Update and rename OapsCopyVals36.js to OapsCopyVals37.js

--- OapsCopyVals37.js
+++ OapsCopyVals37.js
@@ -2,36 +2,24 @@ var demo123CF_CopyFieldValue = new function() {
     var self = this;
 	this.inField = new Number();
 	this.outField = new Number();
-	
 
-    this.setFieldOriginID1 = function ( fieldId1 ) {
-        this.fieldOrigin1 = fieldId1;
-    }
-    
-    this.setFieldDestinationID1 = function ( fieldId1) {
-        this.fieldDestination1 = fieldId1;
-    }
 
     this.init = function () {
-        this.setOnChange(36040704, 44541819);
+        this.setOnChange();
     }
     
-    this.updateDestinationValue1 = function (){
-	$(this.id123cf+this.fieldDestination1).val($(this.id123cf+this.fieldOrigin1).val());
-    }
 
-    this.setOnChange = function(in, out){
+    this.setOnChange = function(){
  
     window.addEventListener('load', function(){
-	console.log('**START3**');
-	    this.inField = in;
-	    this.outField = out;
-	    /**
+	
+	    console.log('**START4**');
+	    
 	    this.inField = 36040704;
 	    this.outField = 44541819;
 	    console.log(this.inField);
-	  **/  
-        var /** sourceControlId = this.inField,  THE ID OF THE SENDER CONTROL */
+
+	    var /** sourceControlId = this.inField,  THE ID OF THE SENDER CONTROL */
          /**    targetControlId = this.outField, THE ID OF THE RECEIVER CONTROL **/
             sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inField),
             targetControlInstance = loader.getEngine().getDocument().getElementById(this.outField);
