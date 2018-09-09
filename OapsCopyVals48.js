f478b4f6971cb9ec99baf2d50a0cb04ec38999d4
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 09:23:05 -0400

    Update and rename OapsCopyVals47.js to OapsCopyVals48.js

--- OapsCopyVals48.js
+++ OapsCopyVals48.js
@@ -13,11 +13,17 @@ var Oaps_CopyFieldValue = new function() {
 	    this.relation = 36936048;
 
 /**Allergies **/
-	    var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inChildName),
-            targetControlInstance = loader.getEngine().getDocument().getElementById(36181440);
+	
+
+        var sourceControlId = 36040704, /** THE ID OF THE SENDER CONTROL */
+            targetControlId = 36181440, /** THE ID OF THE RECEIVER CONTROL **/
+            sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
+            targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+
         sourceControlInstance.on('value-change', function(){
             targetControlInstance.setValue( sourceControlInstance.getValue() );
         });
+
         targetControlInstance.setValue( sourceControlInstance.getValue() );
 
     });
