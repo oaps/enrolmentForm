05c51f042d6807dc6a3f583a28d3f84723763c9a
oaps <35172365+oaps@users.noreply.github.com>
2018-09-09 09:19:20 -0400

    Update and rename OapsCopyVals46.js to OapsCopyVals47.js

---
+++ OapsCopyVals47.js
@@ -0,0 +1,29 @@
+var Oaps_CopyFieldValue = new function() {
+    var self = this;
+	
+    this.init = function (){
+        this.setOnChange();
+    }
+    
+    this.setOnChange = function(){
+ 
+    window.addEventListener('load', function(){
+	    this.inChildName = 36040704;
+	    this.inSignor = 36935922;
+	    this.relation = 36936048;
+
+/**Allergies **/
+	    var sourceControlInstance = loader.getEngine().getDocument().getElementById(this.inChildName),
+            targetControlInstance = loader.getEngine().getDocument().getElementById(36181440);
+        sourceControlInstance.on('value-change', function(){
+            targetControlInstance.setValue( sourceControlInstance.getValue() );
+        });
+        targetControlInstance.setValue( sourceControlInstance.getValue() );
+
+    });
+  }
+}
+
+$( document ).ready(function() {
+    Oaps_CopyFieldValue.init(); 
+});
