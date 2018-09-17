0435b22d154e09849a27fc65b330384b731c1672
oaps <35172365+oaps@users.noreply.github.com>
2018-09-16 22:38:44 -0400

    Update and rename OapsCopyVals106.js to OapsCopyVals107.js

--- OapsCopyVals107.js
+++ OapsCopyVals107.js
@@ -233,14 +233,6 @@ var Oaps_CopyFieldValue = new function() {
 	
 }
 
-this.doFunction = function (){
-	    
-	console.log("**HERE AT GET ELEMENT***");
-}
-
-$( document ).getElementById('#form > div:nth-child(2) > div:nth-child(3) > div > button:nth-child(1)').onclick = doFunction;
-
-
 $( document ).ready(function() {
 	Oaps_CopyFieldValue.init();
 });
